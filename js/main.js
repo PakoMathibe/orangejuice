910a72be4b7c15628b9de898c8af0e8d90200c0c
PakoMathibe <pako.mathibe@gmail.com>
2025-07-24 14:44:40 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,153 @@
+(function ($) {
+    "use strict";
+
+    
+    
+    // Initiate the wowjs
+    new WOW().init();
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 45) {
+            $('.navbar').addClass('sticky-top shadow-sm');
+			$('.navbar').addClass('navbar-scrolled');
+			$('#logo').attr('src', 'img/logo.png');
+        } else {
+            $('.navbar').removeClass('sticky-top shadow-sm');
+			 $('.navbar').removeClass('navbar-scrolled');
+			$('#logo').attr('src', 'img/logo-white.png');
+        }
+    });
+	
+/* 	$(document).ready(function() {
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 50) { // Adjust the scroll threshold as needed
+      $('.navbar').addClass('navbar-scrolled');
+      $('#logo').attr('src', 'img/logo-white.png.png');
+    } else {
+      $('.navbar').removeClass('navbar-scrolled');
+      $('#logo').attr('src', 'img/logo.png');
+    }
+  });
+}); */
+
+
+    // Hero Header carousel
+    $(".header-carousel").owlCarousel({
+        animateOut: 'fadeOut',
+        items: 1,
+        margin: 0,
+        stagePadding: 0,
+        autoplay: true,
+        smartSpeed: 500,
+        dots: true,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-arrow-left"></i>',
+            '<i class="bi bi-arrow-right"></i>'
+        ],
+    });
+	
+	function navigateToLink() {
+        var dropdown = document.getElementById("dropdown");
+        var selectedValue = dropdown.options[dropdown.selectedIndex].value;
+        if (selectedValue) {
+            window.location.href = selectedValue;
+        }
+    }
+
+    // attractions carousel
+    $(".blog-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        center: false,
+        dots: false,
+        loop: true,
+        margin: 25,
+        nav : true,
+        navText : [
+            '<i class="fa fa-angle-right"></i>',
+            '<i class="fa fa-angle-left"></i>'
+        ],
+        responsiveClass: true,
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:2
+            },
+            1200:{
+                items:3
+            }
+        }
+    });
+
+
+    // testimonial carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        center: false,
+        dots: true,
+        loop: true,
+        margin: 25,
+        nav : true,
+        navText : [
+            '<i class="fa fa-angle-right"></i>',
+            '<i class="fa fa-angle-left"></i>'
+        ],
+        responsiveClass: true,
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:2
+            },
+            1200:{
+                items:3
+            }
+        }
+    });
+
+
+    // Facts counter
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 5,
+        time: 2000
+    });
+
+
+   // Back to top button
+   $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+        $('.back-to-top').fadeIn('slow');
+    } else {
+        $('.back-to-top').fadeOut('slow');
+    }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+	
+	
+
+
+
+})(jQuery);
+
